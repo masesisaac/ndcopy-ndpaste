6104b9a986b1a990bb5271f34d4f655677b40498
masesisaac <masesisaac@gmail.com>
2018-11-03 00:59:01 +0300

    cleaned lib

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-#!/usr/bin/env node
+'use strict';
 
-const ncp = require("copy-paste");
+const clipboardy = require('clipboardy');
 const getStdin = require('get-stdin');
 
-const copy = async() => {
-  let string = process.argv[2];
+const copy = async(str, cb) => {
+  let content;
   let stdin = await getStdin();
-  string = stdin || string;
-  if(string){
-    ncp.copy(stdin || string, function (a) {
-      process.exit(0);
-    });
-  }else{
+  content = str || stdin || process.argv[2];
+  if (content){
+    clipboardy.writeSync(content);
+    cb(content);
+    process.exit(0);
+  } else {
     console.log('nothing to copy');
+    cb('nothing to copy');
     process.exit(0);
   }
-}
+};
 
 const paste = () => {
-  process.stdout.write(ncp.paste());
-}
+  let content = clipboardy.readSync();
+  process.stdout.write(content);
+  return content;
+};
 
 module.exports = {copy, paste};
